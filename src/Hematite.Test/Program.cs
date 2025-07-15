7f19e12a24b17a3df8a80994d813feaf9b867927
danil <61111955+danilwhale@users.noreply.github.com>
2025-07-16 00:18:23 +0300

    feat(test): add Platform.SetTarget example

--- src/Hematite.Test/Program.cs
+++ src/Hematite.Test/Program.cs
@@ -1,8 +1,12 @@
-﻿using Hematite.Windowing;
+﻿using Hematite;
+using Hematite.Windowing;
 using Hematite.Windowing.SDL;
 using Vortice.Mathematics;
 
 SdlPlatform.Register();
+
+// try to force SDL3 platform (in case you're registering several platforms)
+Platform.SetTarget(SdlPlatform.Identifier); 
 
 WindowDescriptor descriptor = new(
     Title: "test", 
